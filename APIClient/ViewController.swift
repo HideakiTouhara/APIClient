d704a742274fe388e5e03684531de9ebbacc3bf8
HideakiTouhara <hideee6723@gmail.com>
2019-09-15 23:39:38 +0900

    initial commit

---
+++ APIClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APIClient
+//
+//  Created by HideakiTouhara on 2019/09/15.
+//  Copyright © 2019 HideakiTouhara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
